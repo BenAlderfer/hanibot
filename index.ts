68b3eb38c021f1b0e379f2c6206350f845e30ff9
Ben Alderfer <benalderfer974@gmail.com>
2022-01-05 23:03:18 -0500

    Add a listener for the member added event

--- index.ts
+++ index.ts
@@ -2,10 +2,15 @@ require('dotenv').config();
 
 import { Client } from "discord.js";
 
+// @ts-ignore
 const client = new Client();
 
 client.on("ready", async () => {
     console.info(`${client.user?.username} is online.`);
+});
+
+client.on("GUILD_MEMBER_ADD", async () => {
+    console.info(`member added`);
 });
 
 client.login(process.env.TOKEN);
