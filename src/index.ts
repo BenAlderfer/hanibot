d070a018dd6bdc327471a1df2d69265d88b2f0f9
Ben Alderfer <benalderfer974@gmail.com>
2022-01-06 19:45:48 -0500

    Initial draft that may work

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+require('dotenv').config();
+
+import { Client, Intents } from "discord.js";
+import { welcomeMessage } from "./strings";
+
+// @ts-ignore
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+client.on("ready", async () => {
+    console.info(`${client.user?.username} is online.`);
+});
+
+client.on("guildMemberAdd", async (member) => {
+    console.info(`member added`);
+    await member.send(welcomeMessage);
+    console.info("message sent to user");
+});
+
+client.login(process.env.TOKEN);
---
+++ src/strings.ts
@@ -0,0 +1,6 @@
+export const welcomeMessage = `Welcome!
+Here are some rules:
+- Don't be stupid
+
+Here are some helpful links:
+- google.com`;
