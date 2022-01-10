9e39589299cec1a28713832601b9b78fb84a0582
Ben Alderfer <benalderfer974@gmail.com>
2022-01-10 10:33:00 -0500

    Update welcome message, add deployment instructions

--- README.md
+++ README.md
@@ -13,3 +13,7 @@ TOKEN=<place your token here>
 ## Running the server
 
 `npm run start` is enough to get the server running and connected to discord if you have the right env vars and invited the bot to the server.
+
+## Deploying
+
+Currently, the repo is set to automatically deploy to Heroku. Any new commits to master will get built and deployed.
--- src/strings.ts
+++ src/strings.ts
@@ -1,6 +1,14 @@
-export const welcomeMessage = `Welcome!
-Here are some rules:
-- Don't be stupid
+export const welcomeMessage = `Welcome to RBC’s Young Adults Discord server! We are so happy to have you. Here you won’t miss a beat of what’s happening! Below are some helpful guidelines on how to use our server appropriately and stay up to date with all the activities we enjoy, such as: Bible studies, hiking, game nights, memes, and more! We seek to glorify God in all that we do together, and with that Discord helps us stay connected. Please see the Code of Conduct below, and we’ll see you in the chat!
 
-Here are some helpful links:
-- google.com`;
+Code of Conduct
+
+• “Let no corrupting talk come out of your mouths, but only such as is good for building up, as fits the occasion, that it may give grace to those who hear.” - Ephesians 4:29. In the spirit of this verse, please watch your speech on this server and seek to glorify God with all you write and say. 
+• Please set your username so we know who you are. See “How to change your nickname” in the section below if you are not sure how to do this.
+• There are many channels on this server, please take advantage of them and post to the appropriate channel. If you are not interested in receiving messages for certain channels, feel free to mute them.
+• If you have any problems with someone or something in this community, please reach out to Pastor Jim, Hanibeth, or Larry.
+
+New to Discord? Here are some helpful tips:
+
+• Beginner’s guide: https://support.discord.com/hc/en-us/articles/360045138571-Beginner-s-Guide-to-Discord
+• How to change your nickname: https://support.discord.com/hc/en-us/articles/219070107-Server-Nicknames#h_01EJTEHFA19Q5BK1GQY2XJ2ZJS
+• To setup your microphone or speakers/headphones, follow this section of the beginner’s guide: https://support.discord.com/hc/en-us/articles/360045138571-Beginner-s-Guide-to-Discord#h_9de92bc2-3bca-459f-8efd-e1e2739ca4f4. Your microphone options will be listed under “INPUT DEVICE” and speakers and headphones will be listed under “OUTPUT DEVICE”`;
