af6eda9737dd78f61e79c2c2e7391ce526f5860b
Ben Alderfer <benalderfer974@gmail.com>
2022-10-06 21:22:52 -0500

    Finish prettier setup

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,20 @@
-require('dotenv').config();
+import * as dotenv from 'dotenv';
+dotenv.config();
 
-import { Client, Intents } from "discord.js";
-import { welcomeMessage } from "./strings";
+import { Client, Intents } from 'discord.js';
+import { welcomeMessage } from './strings';
 
 // @ts-ignore
 const client = new Client({ intents: [Intents.FLAGS.GUILD_MEMBERS] });
 
-client.on("ready", async () => {
-    console.info(`${client.user?.username} is online.`);
+client.on('ready', async () => {
+  console.info(`${client.user?.username} is online.`);
 });
 
-client.on("guildMemberAdd", async (member) => {
-    console.info(`member added`);
-    await member.send(welcomeMessage);
-    console.info("message sent to user");
+client.on('guildMemberAdd', async member => {
+  console.info(`member added`);
+  await member.send(welcomeMessage);
+  console.info('message sent to user');
 });
 
 client.login(process.env.TOKEN);
