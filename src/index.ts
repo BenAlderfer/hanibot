93df6021a8c1b46e4162f191b5313cdf5ef4c81e
Ben Alderfer <benalderfer974@gmail.com>
2022-10-06 21:47:04 -0500

    Fix discord v14 breaking changes, improve logging

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,20 @@
 import * as dotenv from 'dotenv';
 dotenv.config();
 
-import { Client, Intents } from 'discord.js';
+import { Client, GatewayIntentBits } from 'discord.js';
 import { welcomeMessage } from './strings';
 
 // @ts-ignore
-const client = new Client({ intents: [Intents.FLAGS.GUILD_MEMBERS] });
+const client = new Client({ intents: [GatewayIntentBits.GuildMembers] });
 
 client.on('ready', async () => {
   console.info(`${client.user?.username} is online.`);
 });
 
 client.on('guildMemberAdd', async member => {
-  console.info(`member added`);
+  console.info(`member added: ${member.user?.username}`);
   await member.send(welcomeMessage);
-  console.info('message sent to user');
+  console.info('message sent to user: ${member.user?.username}');
 });
 
 client.login(process.env.TOKEN);
